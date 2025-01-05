4adb4558d678c7a8fe089d35098d3c621d442e9b
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2025-01-05 07:58:55 +0200

    chore: push latest changes to master

---
+++ nvim/lua/plugins/mapping.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    size = 20,
+    open_mapping = [[<c-\>]],
+    shading_factor = 2,
+    direction = "float", -- Set terminal to float
+    float_opts = {
+      border = "curved",
+    },
+  },
+}
